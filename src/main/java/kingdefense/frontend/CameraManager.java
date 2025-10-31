708e2409c307fb32207d804b9a02eaae5803d5d3
NathanGros <nathan.gros6@gmail.com>
2025-10-31 14:19:55 +0100

    modified camera

--- src/main/java/kingdefense/frontend/CameraManager.java
+++ src/main/java/kingdefense/frontend/CameraManager.java
@@ -20,24 +20,24 @@ public class CameraManager {
 
     public CameraManager() {
         angle = 0.f;
-        distance = 15.f;
+        distance = 20.f;
         targetX = 3.5f;
-        targetY = 0.f;
+        targetY = 1.f;
         targetZ = 3.5f;
         cameraX = targetX + distance * (float) cos(angle);
-        cameraY = targetY + distance;
+        cameraY = targetY + distance * 1.5f;
         cameraZ = targetZ + distance * (float) sin(angle);
         camera = new Camera3D()
             ._position(new Vector3().x(cameraX).y(cameraY).z(cameraZ))
             .target(new Vector3().x(targetX).y(targetY).z(targetZ))
             .up(new Vector3().x(0).y(1).z(0))
             .fovy(distance)
-            .projection(CAMERA_ORTHOGRAPHIC);
+            .projection(CAMERA_PERSPECTIVE);
     }
 
     private void updateCameraPos() {
         cameraX = targetX + distance * (float) cos(angle);
-        cameraY = targetY + distance;
+        cameraY = targetY + distance * 1.5f;
         cameraZ = targetZ + distance * (float) sin(angle);
         camera._position().x(cameraX).y(cameraY).z(cameraZ);
         camera.target().x(targetX).y(targetY).z(targetZ);
@@ -50,16 +50,16 @@ public class CameraManager {
     }
 
     public void zoomIn() {
-        distance /= 1.5f;
-        if (distance < 5.f) distance = 5.f;
+        distance /= 1.1f;
+        if (distance < 20.f) distance = 20.f;
         camera.fovy(distance);
         updateCameraPos();
     }
 
     public void zoomOut() {
-        distance *= 1.5f;
+        distance *= 1.1f;
+        if (distance > 30.f) distance = 30.f;
         camera.fovy(distance);
-        if (distance > 40.f) distance = 40.f;
         updateCameraPos();
     }
 }
